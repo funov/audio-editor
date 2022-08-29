746aa05db8944ce1a12ff3d48c6cbd67b9b01766
Ivan Sychev <funovichf@gmail.com>
2022-08-29 23:25:27 +0500

    fix str time zeros

--- model/utils.py
+++ model/utils.py
@@ -5,14 +5,25 @@ def to_str_time(seconds, minutes, hours):
     if hours > 99:
         raise ValueError
 
-    # TODO 01:02:03 but now 1:2:3
-    return f'{hours}:{minutes}:{seconds}'
+    hours = add_start_zeros(hours)
+    minutes = add_start_zeros(minutes)
+    seconds = add_start_zeros(seconds)
+
+    return hours + ':' + minutes + ':' + seconds
 
 
 def from_str_time_to_int_seconds(str_time):
     list_time = list(map(lambda x: float(x), str_time.split(':')))
 
     return list_time[0] * 60 * 60 + list_time[1] * 60 + list_time[2]
+
+
+def add_start_zeros(float_value):
+    if len(str(int(float_value))) == 1:
+        str_value = '0' + str(float_value)
+        return str_value
+
+    return str(float_value)
 
 
 def convert(from_, to_):
