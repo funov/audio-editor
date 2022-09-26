766b725578ac172107eb3c939da5ee06ec70aca7
Ivan Sychev <funovichf@gmail.com>
2022-09-26 12:46:01 +0500

    Добавил комментарии

--- model/spectrogram.py
+++ model/spectrogram.py
@@ -13,27 +13,53 @@ overlap_fac = 0.5
 fft_size = 2 ** 10
 
 hop_size = np.int32(np.floor(fft_size * (1 - overlap_fac)))
-pad_end_size = fft_size  # the last segment can overlap the end of the data array by no more than one window size
+
+# последний сегмент может перекрывать конец массива данных не более чем на один размер окна
+pad_end_size = fft_size
+
 total_segments = np.int32(np.ceil(len(data) / np.float32(hop_size)))
 t_max = len(data) / np.float32(fs)
 
-window = np.hanning(fft_size)  # our half cosine window
-inner_pad = np.zeros(fft_size)  # the zeros which will be used to double each segment size
+# наше окно с половинным косинусом
+window = np.hanning(fft_size)
 
-proc = np.concatenate((data, np.zeros(pad_end_size)))  # the data to process
-result = np.empty((total_segments, fft_size), dtype=np.float32)  # space to hold the result
+# нули, которые будут использоваться для удвоения размера каждого сегмента
+inner_pad = np.zeros(fft_size)
 
-for i in range(total_segments):  # for each segment
-    current_hop = hop_size * i  # figure out the current segment offset
-    segment = proc[current_hop:current_hop + fft_size]  # get the current segment
-    windowed = segment * window  # multiply by the half cosine function
-    padded = np.append(windowed, inner_pad)  # add 0s to double the length of the data
-    spectrum = np.fft.fft(padded) / fft_size  # take the Fourier Transform and scale by the number of samples
-    auto_power = np.abs(spectrum * np.conj(spectrum))  # find the auto_power spectrum
-    result[i, :] = auto_power[:fft_size]  # append to the results array
+# данные для обработки
+proc = np.concatenate((data, np.zeros(pad_end_size)))
 
-result = 20 * np.log10(result)  # scale to db
-result = np.clip(result, -40, 200)  # clip values
+# пространство для хранения результата
+result = np.empty((total_segments, fft_size), dtype=np.float32)
+
+# для каждого сегмента
+for i in range(total_segments):
+    # определите текущее смещение сегмента
+    current_hop = hop_size * i
+
+    # получить текущий сегмент
+    segment = proc[current_hop:current_hop + fft_size]
+
+    # умножьте на функцию половины косинуса
+    windowed = segment * window
+
+    # добавьте нули, чтобы удвоить длину данных
+    padded = np.append(windowed, inner_pad)
+
+    # возьмите преобразование Фурье и масштабируйте по количеству выборок
+    spectrum = np.fft.fft(padded) / fft_size
+
+    # найдите спектр автоматической мощности
+    auto_power = np.abs(spectrum * np.conj(spectrum))
+
+    # добавить к массиву результатов
+    result[i, :] = auto_power[:fft_size]
+
+# масштабировать до дБ
+result = 20 * np.log10(result)
+
+# обрезать значения
+result = np.clip(result, -40, 200)
 
 result = np.rot90(result)
 result = np.flipud(result)
@@ -48,4 +74,5 @@ plt.xlabel('Время')
 plt.xticks([])
 plt.yticks([])
 
-plt.show()  # plt.imsave()
+# TODO заменить на plt.imsave()
+plt.show()
